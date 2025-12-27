c723dc4b22ded11a5008f009fc93eca4b6116ae3
Dawid <dawid@linek.dev>
2025-12-27 14:02:35 +0100

    feat: csv source

--- epftoolbox2/data/sources/__init__.py
+++ epftoolbox2/data/sources/__init__.py
@@ -3,5 +3,6 @@
 from .base import DataSource
 from .entsoe import EntsoeSource
 from .open_meteo import OpenMeteoSource
+from .csv_source import CsvSource
 
-__all__ = ["DataSource", "EntsoeSource", "OpenMeteoSource"]
+__all__ = ["DataSource", "EntsoeSource", "OpenMeteoSource", "CsvSource"]
