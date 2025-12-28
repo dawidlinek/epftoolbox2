e3a6d76deabe075ecc5783502f7cce4430c74912
Dawid <dawid@linek.dev>
2025-12-28 15:01:00 +0100

    feat: calendar source

--- epftoolbox2/data/sources/__init__.py
+++ epftoolbox2/data/sources/__init__.py
@@ -4,5 +4,6 @@ from .base import DataSource
 from .entsoe import EntsoeSource
 from .open_meteo import OpenMeteoSource
 from .csv import CsvSource
+from .calendar import CalendarSource
 
-__all__ = ["DataSource", "EntsoeSource", "OpenMeteoSource", "CsvSource"]
+__all__ = ["DataSource", "EntsoeSource", "OpenMeteoSource", "CsvSource", "CalendarSource"]
