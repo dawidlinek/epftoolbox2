e14ef25f63711d7cfec1485e3ee620c6d647a70f
Dawid <dawid@linek.dev>
2025-12-31 14:54:34 +0100

    feat: lag transformer

--- epftoolbox2/data/transformers/__init__.py
+++ epftoolbox2/data/transformers/__init__.py
@@ -3,5 +3,6 @@
 from .base import Transformer
 from .timezone import TimezoneTransformer
 from .resample import ResampleTransformer
+from .lag import LagTransformer
 
-__all__ = ["Transformer", "TimezoneTransformer", "ResampleTransformer"]
+__all__ = ["Transformer", "TimezoneTransformer", "ResampleTransformer", "LagTransformer"]
